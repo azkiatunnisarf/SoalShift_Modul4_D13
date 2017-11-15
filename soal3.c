18736ff4d4072a729a885fa1aa086f1fcfbda1ca
azkiatunnisarf <azisarf9@gmail.com>
2017-11-16 01:40:15 +0700

    satutiga

--- soal3.c
+++ soal3.c
@@ -32,6 +32,31 @@ if(res == 1) return -errno;
 return 0;
 }
 
+//read direktori downlads
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
+DIR *dp;
+struct dirent *de;
+char dpath[200];
+sprintf(dpath,"%s%s",dirnyaa,path);
+
+(void) offset;
+(void) fi;
+
+dp = opendir(dpath);
+if(dp == NULL) return -errno;
+
+	while((de = readdir(dp))!=NULL){
+	struct stat st;
+	memset(&st,0,sizeof(st));
+	st.st_ino = de->d_ino;
+	st.st_mode = de->d_type << 12;
+	if(filler(buf, de->d_name, &st, 0)) break;
+	}
+closedir(dp);
+return 0;
+}
+
+
 //akses file
 static int xmp_access(const char *path, int mask){
 int res;
@@ -63,11 +88,9 @@ if(res == -1) return -errno;
 return 0;
 }
 
-
 //open file
 static int xmp_open(const char *path, struct fuse_file_info *fi){
 int res;
-
 
 res = open(dpath, fi->flags);
 if(res == -1) return -errno;
@@ -78,7 +101,6 @@ return 0;
 //read file(read doang)
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
 int fd, res;
-
 
 (void) fi;
 fd = open(path, O_RDONLY);
@@ -102,4 +124,10 @@ if(res == -1) res = -errno;
 
 close(fd);
 return res;
+}
+
+//main program
+int main(int argc, char *argv[]){
+umask(0);
+return fuse_main(argc, argv, &xmp_oper, NULL);
 }
